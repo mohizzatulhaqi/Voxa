330045e4a5cf1af9db6246f3541f8b0094486948
dnthajfry <jefryantara041@gmail.com>
2025-06-16 22:55:26 +0700

    perbaiki breadcrum

--- src/app/laporan-kejadian/components/HeroSection.tsx
+++ src/app/laporan-kejadian/components/HeroSection.tsx
@@ -9,13 +9,8 @@ import Breadcrumb from "@/components/Breadcrumb"
 
 export function HeroSection() {
   return (
-    <div className="flex justify-between items-center mb-8">
-      <Link href="/">
-        <Button variant="outline" size="icon" className="bg-white rounded-full h-12 w-12">
-          <ArrowLeft className="text-blue-900" />
-        </Button>
-      </Link>
-      <Breadcrumb  />
+    <div className="flex justify-end mb-4">
+        <Breadcrumb />
     </div>
   )
 }
--- src/app/riwayat/components/HeroSection.tsx
+++ src/app/riwayat/components/HeroSection.tsx
@@ -6,18 +6,10 @@ import Breadcrumb from "@/components/Breadcrumb";
 export function HeroSection() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
-        <Link href="/">
-          <Button
-            variant="outline"
-            size="icon"
-            className="bg-white rounded-full h-12 w-12 hover:bg-gray-100"
-          >
-            <ArrowLeft className="text-blue-900" />
-          </Button>
-        </Link>
-        <Breadcrumb />
-      </div>
+      <div className="flex justify-end mb-4">
+          <Breadcrumb />
+        </div>
+        
 
       <div className="text-center mb-12">
         <h1 className="text-white text-3xl md:text-4xl font-bold mb-4">
